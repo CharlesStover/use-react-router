04ead7f02d168458ff27e7e32190e3283d60f41e
Charles Stover <github@charlesstover.com>
2019-05-25 17:02:01 -0700

    resolves #16
    resolves #15

--- src/use-react-router.ts
+++ src/use-react-router.ts
@@ -1,30 +1,41 @@
-// @ts-ignore
-import { useContext, useEffect } from 'react';
-
-// @ts-ignore
-import { __RouterContext, RouteComponentProps } from 'react-router';
+import { LocationState } from 'history';
+import { Context, useContext, useEffect } from 'react';
+import { __RouterContext, RouteComponentProps, StaticContext } from 'react-router';
 import useForceUpdate from 'use-force-update';
 
-interface UseRouter {
-  <T = {}>(): RouteComponentProps<T>;
-}
+type AnyContext = Context<RouteComponentProps<any, any, any>>;
 
-interface VoidFunction {
-  (): void;
-}
+const INCORRECT_VERSION_ERROR: Error =
+  new Error('use-react-router may only be used with react-router@^5.');
 
-const useRouter: UseRouter = <T = {}>(): RouteComponentProps<T> => {
-  const forceUpdate: VoidFunction = useForceUpdate();
-  const routerContext: RouteComponentProps<T> = useContext(__RouterContext);
-  if (!routerContext) {
-    throw new Error('use-react-router may only be used with react-router@^5.');
+const MISSING_CONTEXT_ERROR: Error =
+  new Error('useReactRouter may only be called within a <Router /> context.');
+
+export default function useRouter<
+  P extends { [K in keyof P]?: string } = {},
+  C extends StaticContext = StaticContext,
+  S = LocationState,
+>(): RouteComponentProps<P, C, S> {
+
+  // If this version of react-router does not support Context,
+  if (!__RouterContext) {
+    throw INCORRECT_VERSION_ERROR;
   }
+
+  // If the react-router Context is not a parent Component,
+  const context: RouteComponentProps<P, C, S> =
+    useContext<RouteComponentProps<P, C, S>>(
+      __RouterContext as AnyContext as Context<RouteComponentProps<P, C, S>>,
+    );
+  if (!context) {
+    throw MISSING_CONTEXT_ERROR;
+  }
+
+  const forceUpdate: VoidFunction = useForceUpdate();
   useEffect(
     (): VoidFunction =>
-      routerContext.history.listen(forceUpdate),
-    [ routerContext ]
+      context.history.listen(forceUpdate),
+    [ context ],
   );
-  return routerContext;
-};
-
-export default useRouter;
+  return context;
+}
